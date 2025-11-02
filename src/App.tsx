19490c0f33dd2244f72056e7ffa4fcb35d5feba5
AngelicaGFlores <flores.guada.angelica@gmail.com>
2025-11-02 11:24:29 -0800

    this is the module 9 work for idk what

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+    <div className="flex flex-col items-center bg-zinc-900 text-white h-screen">
+      <h1 className="text-5xl pt-10">Task Manager Dashboard</h1>
+    </div>
+  );
+}
---
+++ src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,16 @@
+
+
+
+
+function Dashboard() {
+    return (
+        <div>
+            <h2>Dashboard</h2>
+
+            <div>
+                <h3>stats</h3>
+                <div></div>
+            </div>
+        </div>
+    )
+}
